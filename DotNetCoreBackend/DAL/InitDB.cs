e7df1bd6768fc0266bf3b47464c784ea0cffda90
Jamie Maple <jamiemaple007@gmail.com>
2019-01-04 18:22:37 +0800

    chore: rm dotnet backend version

--- DotNetCoreBackend/DAL/InitDB.cs
+++
@@ -1,3 +0,0 @@
-namespace DotNetCoreBackend.DAL
-{
-}
--- DotNetCoreBackend/Security/Role.cs
+++
@@ -1,9 +0,0 @@
-namespace DotNetCoreBackend.Security
-{
-    public static class Role
-    {
-        public static string WaiterRole = "waiter";
-        public static string AdminRole = "admin";
-        public static string RootRole = "root";
-    }
-}
