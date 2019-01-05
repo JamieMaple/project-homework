b3f30d420e034dfaf2704ba2e594e48d82854ba5
Jamie Maple <jamiemaple007@gmail.com>
2019-01-05 15:34:25 +0800

    fix(name): fix typo name

--- DotNetCoreBackend/GraphQLSchema/RootMutation.cs
+++ DotNetCoreBackend/GraphQLSchema/RootMutation.cs
@@ -9,7 +9,7 @@ namespace DotNetCoreBackend.GraphQLSchema
     {
         public RootMutation()
         {
-            Name = "adminMutation";
+            Name = "RootMutation";
             Description = "管理员修改模块, 包含副作用，均需要 user token （特权级别：admin）";
             this.AuthorizeWith(Policy.AdminPolicy);
 
